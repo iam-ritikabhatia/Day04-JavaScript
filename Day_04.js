80a4bf922a8e897ab2e89b51f8309d05a6fc7738
Ritika Bhatia <139344947+iam-ritikabhatia@users.noreply.github.com>
2024-01-04 21:47:15 +0530

    Add files via upload

---
+++ Day_04.js
@@ -0,0 +1,50 @@
+// #1 (Comparision Different DataTypes)
+
+// console.log("(In case of '2' == 2)", "2" == 2)
+
+// #2
+
+// console.log("(In case of 'Hello'==2)", "Hello" == "2 Years Old" )
+
+// #3 (output is true because H has greater unicode  value that 2)
+// console.log("(In case of 'Hello' > '2')", "Hello" > '2')
+
+// #4
+// console.log("(In case of Number('2 Years old') < 2)"), Number("2 years old") < 2
+
+// #5(NaN not a number in case of NaN, always output is false in every case)
+// console.log(NaN == 2)
+// console.log(NaN > 2)
+// console.log(NaN < 2)
+
+// #6 
+// null case
+// console.log(null == 0)
+// console.log(null != 0)
+// console.log(null <= 0)
+// console.log(null >= 0)
+// console.log(null < 0)
+// console.log(null > 0)
+
+// #7
+// undefined case
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+// console.log(undefined !=0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+// #8
+// console.log(undefined == null)
+// console.log(undefined >= null)
+// console.log(undefined <= null)
+// console.log(undefined <= null)
+// console.log(undefined != null)
+// console.log(undefined > null)
+// console.log(undefined < null)
+
+
+
+
+
